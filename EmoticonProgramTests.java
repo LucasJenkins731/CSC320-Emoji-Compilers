38ec0adebfd859e28da5b5d7e631fdac69143442
ConnorRyan313 <crryan3@quinnipiac.edu>
2025-10-29 11:11:01 -0400

    Example4 file

--- EmoticonProgramTests.java
+++ EmoticonProgramTests.java
@@ -2,14 +2,8 @@ import java.util.*;
 public class EmoticonProgramTests {
   public static void main(String[] args) throws Exception {
     Scanner in = new Scanner(System.in);
-    double testInt1 = 67.0;
-    double testInt2 = 41.0;
-    double testFloat = "23.2222";
-    double testString = "hey";
-    double testChar = ';
-    System.out.println(testInt1);
-    System.out.println(testInt2);
-    System.out.println(108.0);
-    System.out.println(testString);
+    double y = 20.0;
+    double x = 10.0;
+    System.out.println(30.0);
   }
 }
