6ec1824c7786517d1cdb513245689c244c275069
piecon18 <piecon1805@gmail.com>
2025-11-17 12:58:59 -0500

    start

--- EmoticonProgramTests.java
+++ EmoticonProgramTests.java
@@ -2,15 +2,9 @@ import java.util.*;
 public class EmoticonProgramTests {
   public static void main(String[] args) throws Exception {
     Scanner in = new Scanner(System.in);
-    String name = "";
-    name = in.nextLine();
-    float radius = 0.0f;
-    radius = in.nextFloat();
-    float pi = 3.14f;
-    float circumference = 2 * pi * radius;
-    float area = pi * radius * radius;
-    System.out.println(name);
-    System.out.println(circumference);
-    System.out.println(area);
   }
+  public static float multiply(float x) {
+    return x * 2;
+  }
+
 }
